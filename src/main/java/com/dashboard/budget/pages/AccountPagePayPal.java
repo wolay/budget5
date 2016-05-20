f9a8c50047d00e18a3eee3adc8858d2b5f279936
Andrei Anpilogov <aianitro@gmail.com>
2016-05-20 08:07:25 -0700

    Bank accounts: PayPal: ad fix

--- src/main/java/com/dashboard/budget/pages/AccountPagePayPal.java
+++ src/main/java/com/dashboard/budget/pages/AccountPagePayPal.java
@@ -3,6 +3,7 @@ package com.dashboard.budget.pages;
 import static com.dashboard.budget.Util.convertStringAmountToDouble;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 import com.dashboard.budget.DataHandler;
 import com.dashboard.budget.Util;
@@ -16,9 +17,13 @@ public class AccountPagePayPal extends AccountPage {
 
 	@Override
 	public Double getTotal() {
+		// skip advertisement
+		WebElement ad = webDriver.findElement(By.linkText("Proceed to Account Overview"));
+		if (ad != null)
+			ad.click();
+
 		String balanceStr = webDriver.findElement(By.cssSelector("span.vx_h2.enforceLtr  ")).getText();
 		return Util.wrapAmount(convertStringAmountToDouble(balanceStr));
 	}
-
 
 }
