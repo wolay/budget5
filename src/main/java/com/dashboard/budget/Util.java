4560bcd7c8c4dd6b6ca66c3bd8c34da18332d025
Andrei Anpilogov <aianitro@gmail.com>
2017-02-07 23:04:04 -0800

    Fixing timestamps issue in util functions

--- src/main/java/com/dashboard/budget/Util.java
+++ src/main/java/com/dashboard/budget/Util.java
@@ -351,6 +351,7 @@ public class Util implements Config {
 		cal.set(Calendar.HOUR_OF_DAY, 0);
 		cal.set(Calendar.MINUTE, 0);
 		cal.set(Calendar.SECOND, 0);
+		cal.set(Calendar.MILLISECOND, 0);
 		return cal.getTime();
 	}
 
@@ -362,6 +363,7 @@ public class Util implements Config {
 		cal.set(Calendar.HOUR_OF_DAY, 0);
 		cal.set(Calendar.MINUTE, 0);
 		cal.set(Calendar.SECOND, 0);
+		cal.set(Calendar.MILLISECOND, 0);
 		return cal.getTime();
 	}
 
@@ -373,6 +375,7 @@ public class Util implements Config {
 		cal.set(Calendar.HOUR_OF_DAY, 23);
 		cal.set(Calendar.MINUTE, 59);
 		cal.set(Calendar.SECOND, 59);
+		cal.set(Calendar.MILLISECOND, 999);
 		return cal.getTime();
 	}
 
