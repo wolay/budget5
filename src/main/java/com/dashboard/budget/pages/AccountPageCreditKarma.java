022abba647825882001be759b2fb7e1c96a2fc00
Andrei Anpilogov <aianitro@gmail.com>
2017-07-09 02:56:41 -0700

    Credit Karma: refactored to make robust +  added logs

--- src/main/java/com/dashboard/budget/pages/AccountPageCreditKarma.java
+++ src/main/java/com/dashboard/budget/pages/AccountPageCreditKarma.java
@@ -1,7 +1,7 @@
 package com.dashboard.budget.pages;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.openqa.selenium.By;
 
@@ -10,12 +10,13 @@ import com.dashboard.budget.Util;
 import com.dashboard.budget.DAO.Account;
 import com.dashboard.budget.DAO.Label;
 import com.dashboard.budget.DAO.PageElementNotFoundException;
+import com.google.common.collect.Sets;
 
 public class AccountPageCreditKarma extends AccountPage {
 
 	private Label lblTransUnion;
 	private Label lblEquifax;
-	private final List<Integer> staticCreditScores = Arrays.asList(new Integer[] { 580, 640, 700, 750, 300, 850 });
+	private Set<Integer> staticCreditScores = Sets.newHashSet(580, 640, 700, 750, 300, 850);
 
 	public AccountPageCreditKarma(Account account, DataHandler dataHandler) {
 		super(account, dataHandler);
@@ -40,18 +41,36 @@ public class AccountPageCreditKarma extends AccountPage {
 	}
 
 	public int getScore() throws PageElementNotFoundException {
-		return (identifyScore(lblTransUnion.getText()) + identifyScore(lblEquifax.getText())) / 2;
+		// TransUnion
+		String rawTransUnion = lblTransUnion.getText();
+		logger.info("Parsing 'TransUnion' credit score.. '{}' ", rawTransUnion);
+		
+		Integer csTransUnion = identifyScore(rawTransUnion);
+		logger.info("Identified 'TransUnion' credit score = {} ", csTransUnion);
+		
+		// Equifax
+		String rawEquifax = lblEquifax.getText(); 
+		logger.info("Parsing 'Equifax' credit score.. '{}' ", rawEquifax);
+		
+		Integer csEquifax = identifyScore(rawEquifax);
+		logger.info("Identified 'Equifax' credit score = {} ", csEquifax);
+		
+		// Average credit score
+		return (csTransUnion + csEquifax) / 2;
 	}
 
 	private Integer identifyScore(String rawData) {
+		Set<Integer> staticScores = new HashSet<Integer>(staticCreditScores);
 		for (String csStr : rawData.split("\\n")) {
 			if (!Util.isNumeric(csStr))
 				continue;
 			Integer csInt = Integer.valueOf(csStr);
 			if (csInt < 300 || csInt > 850)
 				continue;
-			if (staticCreditScores.contains(csInt))
+			if (staticScores.contains(csInt)){
+				staticScores.remove(csInt);
 				continue;
+			}
 			return csInt;
 		}
 		return null;
